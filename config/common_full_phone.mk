3bb0407ba7ba0b88fe7696afc65d9bf3be67dbd9
Vineel Sai <vineelsai26@gmail.com>
2020-10-14 06:54:50 +0000

    initial cheatos vendor

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,11 @@
 # Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+$(call inherit-product, vendor/cheatos/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
 # Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/cheatos/overlay/dictionaries
 
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/cheatos/config/telephony.mk)
